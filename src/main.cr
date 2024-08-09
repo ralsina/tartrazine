89d212b71cbee12085fa656cfc4468fd9602ff0b
Roberto Alsina <roberto.alsina@gmail.com>
2024-08-09 16:53:24 -0300

    Start actual CLI

--- src/main.cr
+++ src/main.cr
@@ -1,5 +1,26 @@
 require "./**"
 
+HELP = <<-HELP
+tartrazine: a syntax highlighting tool
+
+Usage:
+
+  tartrazine FILE -f html [-t theme][--standalone][--line-numbers]
+                          [-l lexer] [-o output][--css]
+  tartrazine FILE -f terminal [-t theme][-l lexer][-o output]
+  tartrazine FILE -f json [-o output]
+  tartrazine --list-themes
+  tartrazine --list-lexers
+
+-f <formatter>      Format to use (html, terminal, json)
+-t <theme>          Theme to use (see --list-themes)
+-l <lexer>          Lexer (language) to use (see --list-lexers)
+-o <output>         Output file (default: stdout)
+--standalone        Generate a standalone HTML file
+--css               Generate a CSS file for the theme
+--line-numbers      Include line numbers in the output
+HELP
+
 lexer = Tartrazine.lexer("crystal")
 theme = Tartrazine.theme(ARGV[1])
 formatter = Tartrazine::Html.new
