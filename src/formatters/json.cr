fa647e898a4d1e48a41a5e17d7daf235da540fb1
Roberto Alsina <roberto.alsina@gmail.com>
2024-08-19 10:15:02 -0300

    Make formatter a bit more convenient

--- src/formatters/json.cr
+++ src/formatters/json.cr
@@ -4,9 +4,11 @@ module Tartrazine
   class Json < Formatter
     property name = "json"
 
-    def format(text : String, lexer : Lexer, outp : IO) : Nil
+    def format(text : String, lexer : Lexer, io : IO?) : String?
+      outp = io.nil? ? String::Builder.new("") : io
       tokenizer = Tokenizer.new(lexer, text)
       outp << Tartrazine::Lexer.collapse_tokens(tokenizer.to_a).to_json
+      return outp.to_s if io.nil?
     end
   end
 end
