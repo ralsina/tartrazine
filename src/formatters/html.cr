4a598a575bf904ccef29daceaff712a5acf4e11f
Roberto Alsina <roberto.alsina@gmail.com>
2024-08-19 11:18:54 -0300

    Make formatter a bit more convenient

--- src/formatters/html.cr
+++ src/formatters/html.cr
@@ -34,7 +34,7 @@ module Tartrazine
                    @weight_of_bold : Int32 = 600)
     end
 
-    def format(text : String, lexer : Lexer, io : IO) : Nil
+    def format(text : String, lexer : Lexer, io : IO?) : String?
       outp = io.nil? ? String::Builder.new("") : io
       pre, post = wrap_standalone
       outp << pre if standalone?
