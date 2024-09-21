766f9b4708972c4ecb8e1a613a60d1cd57fc7c9a
Roberto Alsina <roberto.alsina@gmail.com>
2024-09-21 14:08:07 -0300

    chore: Improve changelog handling

--- do_release.sh
+++ do_release.sh
@@ -7,7 +7,7 @@ VERSION=$(git cliff --bumped-version --unreleased |cut -dv -f2)
 sed "s/^version:.*$/version: $VERSION/g" -i shard.yml
 git add shard.yml
 hace lint test
-git cliff --bump -o
+git cliff --bump -u -p CHANGELOG.md
 git commit -a -m "bump: Release v$VERSION"
 git tag "v$VERSION"
 git push --tags
